3b3d48832648a2e555ec4fc9b2bb94ed1aa6cde2
dsypien <davidsypien@gmail.com>
2013-11-02 12:56:11 -0400

    Add PDF Manager, set up project settings

---
+++ PDFManager/PDFManager/PDFManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF2Text.PDFCommon
+{
+    public class Class1
+    {
+    }
+}
