b82cef5dfb975ac8b21c179fe7c06bd99eed95c5
dsypien <davidsypien@gmail.com>
2013-11-03 12:58:31 -0500

    Implement GetPDFText

--- PDFManager/PDFManager/PDFManager.cs
+++ PDFManager/PDFManager/PDFManager.cs
@@ -4,10 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+
 namespace PDF2Text.PDFCommon
 {
     public class PDFManager
     {
+        public string GetPDFText(string filename)
+        {
+            PDDocument pdoc = PDDocument.load(filename);
+            PDFTextStripper txtStripper = new PDFTextStripper();
+            string text = txtStripper.getText(pdoc);
+            pdoc.close();
 
+            return text;
+        }
     }
 }
